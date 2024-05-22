b481fd3bbe8bfaaf40f63460744a6c91acbc8778
Justin Stitt <justinstitt@google.com>
2024-05-22 01:36:52 +0000

    better identification using better apis :>]
    Signed-off-by: Justin Stitt <justinstitt@google.com>

--- llvm/include/llvm/Transforms/Utils/IdiomExclusions.h
+++ llvm/include/llvm/Transforms/Utils/IdiomExclusions.h
@@ -2,11 +2,16 @@
 #define LLVM_TRANSFORMS_UTILS_IDIOMEXCL_H
 
 #include "llvm/IR/Analysis.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/PassManager.h"
 
 namespace llvm {
 
 class IdiomExclusionsPass : public PassInfoMixin<IdiomExclusionsPass> {
+private:
+  SmallVector<WithOverflowInst *>
+  checkOverflowIntstructions(Function &F, FunctionAnalysisManager &AM);
+
 public:
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
 };
--- llvm/lib/Transforms/Utils/IdiomExclusions.cpp
+++ llvm/lib/Transforms/Utils/IdiomExclusions.cpp
@@ -1,146 +1,135 @@
 #include "llvm/Transforms/Utils/IdiomExclusions.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Analysis/ValueTracking.h"
 #include "llvm/IR/Analysis.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/InstIterator.h"
 #include "llvm/IR/Intrinsics.h"
 #include "llvm/IR/PassManager.h"
 
 using namespace llvm;
 
-static bool IsSignedAddOverflowIntrinsic(const IntrinsicInst *II) {
-  return II->getIntrinsicID() == Intrinsic::sadd_with_overflow;
+namespace {
+  bool isAddOverflowIntrinsic(const WithOverflowInst *WOI) {
+    if (!WOI) return false;
+
+    return WOI->getIntrinsicID() == Intrinsic::sadd_with_overflow ||
+           WOI->getIntrinsicID() == Intrinsic::uadd_with_overflow;
+  }
+
+  bool isAPlusBCMPAOrB(BasicBlock *BB, ExtractValueInst *Sum, WithOverflowInst *WOI) {
+    assert(BB);
+    assert(Sum);
+    assert(WOI);
+
+    if (!Sum->isUsedInBasicBlock(BB))
+      return false;
+
+    for (const User *SU : Sum->users()) {
+      if (!dyn_cast<ICmpInst>(SU))
+        return false;
+
+      const Value *OtherValue;
+
+      for (unsigned Idx = 0; Idx < SU->getNumOperands(); ++Idx)
+        if (SU->getOperand(Idx) != Sum)
+          OtherValue = SU->getOperand(Idx);
+
+      // Determine if OtherValue was a constituent of the Sum to begin with...
+      // Easy, OtherValue is equal to one of the addends
+      if (is_contained(WOI->args(), OtherValue))
+        continue;
+      // Harder... OtherValue may be a 'load' or 'phi' instruction
+      if (const PHINode *Phi = dyn_cast<PHINode>(OtherValue)) {
+        if (is_contained(WOI->args(),
+                         Phi->getIncomingValueForBlock(WOI->getParent()))) {
+          continue;
+        }
+      } else if (const LoadInst *OtherL = dyn_cast<LoadInst>(OtherValue)) {
+        /* errs() << "OtherValue was a LoadInst!\n"; */
+        auto PointerOperandMatches = [&](const Use &U) {
+          const LoadInst *L = dyn_cast<LoadInst>(&U);
+          return L && OtherL->getPointerOperand() == L->getPointerOperand();
+        };
+
+        if (any_of(WOI->args(), PointerOperandMatches))
+          continue;
+      }
+      return false;
+    }
+
+    return true;
+  }
+
+} // namespace
+
+/* Return the Overflow intrinsics that can be removed */
+SmallVector<WithOverflowInst *> IdiomExclusionsPass::checkOverflowIntstructions(Function &F,
+                                                     FunctionAnalysisManager &AM) {
+  // TODO: Expand to sub for stuff like "while (x--)"
+  SmallVector<WithOverflowInst *> AddOverflowInstrinsics;
+  SmallVector<WithOverflowInst *> RemovableOverflowIntrinsics;
+
+  // Locate the CallInstr's, looking specifically for add overflow intrinsics
+  for (Instruction &Inst : instructions(F)) {
+    WithOverflowInst *WOI = dyn_cast<WithOverflowInst>(&Inst);
+    if (WOI && isAddOverflowIntrinsic(WOI)) {
+      AddOverflowInstrinsics.push_back(WOI);
+    }
+  }
+
+  if (AddOverflowInstrinsics.empty())
+    return RemovableOverflowIntrinsics;
+
+  errs() << "AddOverflowInstrinsics.size(): " << AddOverflowInstrinsics.size() << "\n";
+
+  for (WithOverflowInst *WOI : AddOverflowInstrinsics) {
+    ExtractValueInst *Sum = nullptr;
+    ExtractValueInst *Overflow = nullptr;
+
+    for (User *U : WOI->users()) {
+      if (auto *EVI = dyn_cast<ExtractValueInst>(U)) {
+        assert(EVI->getNumIndices() == 1 && "This aggregate should only have 1 index");
+        if (EVI->getIndices()[0] == 0)
+          Sum = EVI;
+        else if (EVI->getIndices()[0] == 1) {
+          Overflow = EVI;
+        }
+      }
+    }
+
+    auto OverflowUserMatchesIdiom = [&](const User *U) {
+      const BranchInst *B = dyn_cast<BranchInst>(U);
+      assert(B->getNumSuccessors() == 2 && "Overflow path and no overflow path");
+      if (!B) return true;
+      return isAPlusBCMPAOrB(B->getSuccessor(1), Sum, WOI);
+    };
+
+    if (all_of(Overflow->users(), OverflowUserMatchesIdiom))
+      RemovableOverflowIntrinsics.push_back(WOI);
+
+    assert(Sum && Overflow && "These should've been set if WOI had valid users");
+  }
+
+  return RemovableOverflowIntrinsics;
 }
+
 
 PreservedAnalyses IdiomExclusionsPass::run(Function &F,
                                            FunctionAnalysisManager &AM) {
-  errs() << "F: " << F.getName() << "\n";
+  SmallVector<WithOverflowInst *> Removable = checkOverflowIntstructions(F, AM);
 
-  const IntrinsicInst *AddIntrinsic = nullptr;
-
-  std::vector<BasicBlock *> BasicBlocks;
-  for (BasicBlock &BB : F) {
-    /* const Module *Module = BB.getModule(); */
-    errs() << "\\--BB: " << BB.getName() << "\n";
-    for (const Instruction &Inst : BB) {
-      Inst.dump();
-      /* if (AddIntrinsic) { */
-      /*   errs() << "Found AddIntrinsic!\n"; */
-      /* } */
-      if (auto *II = dyn_cast<IntrinsicInst>(&Inst)) {
-        if (IsSignedAddOverflowIntrinsic(II)) {
-          AddIntrinsic = II;
-          break;
-        }
-        /* errs() << "^^^ calling: " << II->getCalledOperand()->getName() << "\t" */
-        /*   << II->getIntrinsicID() << "\t" << Intrinsic::sadd_with_overflow << */
-        /*   "\t" << II->getNameOrAsOperand() << "\t" <<  II->getName() << "\n"; */
-      }
-    }
+  errs() << "Count of removable WOI's: " << Removable.size() << "\n";
+  for (const WithOverflowInst *WOI : Removable) {
+    WOI->dump();
+    errs() << "---\n";
   }
 
-  // this seems to get the operands that feed into the add intrinsic
-  if (!AddIntrinsic) {
-    errs() << "No AddIntrinsic found!\n";
+  if (Removable.empty())
     return PreservedAnalyses::all();
-  }
-  errs() << "End BB\n";
-  errs() << "NumOperands: " << AddIntrinsic->getNumOperands() << "\n";
-  /* const Use *IntrinsicUses = AddIntrinsic->getOperandList(); */
-  for (unsigned i = 0; i < AddIntrinsic->getNumOperands(); ++i) {
-    const Value *InstrinsicOperandValue = AddIntrinsic->getOperandList()[i];
-    /* const Use *IntrinsicUse = &IntrinsicUses[i]; */
-    /* const Value *IntrinsicUseValue = IntrinsicUse->get(); */
-    errs() << "IntrinsicUseValue->dump(): ";
-    InstrinsicOperandValue->dump();
-  }
 
-
-
-  const Value *SumValue = nullptr;
-  const Value *OverflowValue = nullptr;
-
-  for (const Use &IntrinsicUse : AddIntrinsic->uses()) {
-
-    /* const Instruction *Instr = dyn_cast<Instruction>(IntrinsicUse.getUser()); */
-    /* errs() << "Instr: "; */
-    /* Instr->dump(); */
-
-    const ExtractValueInst *EVI =
-      dyn_cast<ExtractValueInst>(IntrinsicUse.getUser());
-
-    EVI->dump();
-
-    if (!EVI) {
-      errs() << "Using Overflow Intrinsic result in non-extractvalue instruction!\n";
-      return PreservedAnalyses::all();
-    }
-
-    ArrayRef<unsigned> Indices = EVI->getIndices();
-
-    if (Indices.empty()) {
-      errs() << "No indices are present on extractvalue instruction!\n";
-      return PreservedAnalyses::all();
-    }
-
-    switch (Indices[EVI->getAggregateOperandIndex()]) {
-      case 0:
-        SumValue = IntrinsicUse.getUser();
-        break;
-      case 1:
-        OverflowValue = IntrinsicUse.getUser();
-        break;
-      default:
-        errs() << "bad index in extractvalue!\n";
-        return PreservedAnalyses::all();
-    }
-
-  }
-
-  if (!SumValue || !OverflowValue) {
-    errs() << "Didn't find one of SumValue or OverflowValue!\n";
-    return PreservedAnalyses::all();
-  }
-
-  errs() << "SumValue: "; SumValue->dump();
-  errs() << "OverflowValue: "; OverflowValue->dump();
-
-  const BasicBlock *BBSucc = nullptr;
-
-  for (const User *OverflowUser : OverflowValue->users()) {
-    if (const BranchInst *BI = dyn_cast<BranchInst>(OverflowUser)) {
-      if (BI->getNumSuccessors() != 2) continue;
-      BBSucc = BI->getSuccessor(1); // If no overflow
-    }
-  }
-
-  if (!BBSucc) {
-    errs() << "Didn't find BasicBlock successor for overflow case!\n";
-    return PreservedAnalyses::all();
-  }
-
-  errs() << "BBSucc->dump(): "; BBSucc->dump();
-
-  const PHINode *Phi = dyn_cast<PHINode>(BBSucc->getIterator()->begin());
-
-  if (!Phi) {
-    errs() << "Failed to convert first instruction of BBSucc to Phi node!\n";
-    return PreservedAnalyses::all();
-  }
-
-  if(const ICmpInst *Comp = dyn_cast<ICmpInst>(BBSucc->getFirstNonPHI())) {
-    /* Signed Less Than or Unsigned Less Than */
-    if (Comp->getSignedPredicate() == ICmpInst::Predicate::ICMP_SLT ||
-        Comp->getSignedPredicate() == ICmpInst::Predicate::ICMP_ULT) {
-      bool LHSMatchesSum = Comp->getOperand(0) == SumValue;
-      bool RHSMatchesPhi = Comp->getOperand(1) == Phi;
-      errs() << "Same Operand{0}: " << LHSMatchesSum << "\n";
-      errs() << "Same Operand{1}: " << RHSMatchesPhi << "\n";
-      if (LHSMatchesSum && RHSMatchesPhi) {
-        errs() << "Found IDIOM EXCLUSION matching if (a + b < a)\n";
-      }
-    }
-  }
-
-  return PreservedAnalyses::all();
+  // TODO: otherwise, if we removed stuff we need to invalidate some analyses
+  return PreservedAnalyses::all(); // ... but assume we don't for now
 }
 
