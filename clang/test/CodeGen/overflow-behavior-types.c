745625fe6e1525f1fe079439097daf1a75d6854d
Justin Stitt <justinstitt@google.com>
2025-05-02 16:42:48 -0700

    tests wip
    Signed-off-by: Justin Stitt <justinstitt@google.com>

--- clang/test/CodeGen/overflow-behavior-types.c
+++ clang/test/CodeGen/overflow-behavior-types.c
@@ -1,8 +1,99 @@
-// RUN: %clang_cc1 -triple x86_64-linux-gnu %s -fsanitize=signed-integer-overflow,unsigned-integer-overflow -emit-llvm -o - | FileCheck %s --implicit-check-not='with.overflow'
+// RUN: %clang_cc1 -triple x86_64-linux-gnu %s -fsanitize=signed-integer-overflow,unsigned-integer-overflow,implicit-signed-integer-truncation,implicit-unsigned-integer-truncation -emit-llvm -o - | FileCheck %s --check-prefix=DEFAULT
 
 #define __wrap __attribute__((overflow_behavior("wrap")))
 #define __nowrap __attribute__((overflow_behavior("no_wrap")))
 
-void test1(int __wrap a) {
+// DEFAULT-LABEL: define {{.*}} @test1
+void test1(int __wrap a, int __nowrap b) {
+  // DEFAULT: add i32
   (a + 1);
+
+  // DEFAULT: llvm.sadd.with.overflow.i32
+  (b + 1);
+
+  // DEFAULT: sub i32 0
+  (-a);
+
+  // DEFAULT: llvm.ssub.with.overflow.i32
+  (-b);
+
+  // DEFAULT: add i32
+  a++;
+  // DEFAULT: llvm.sadd.with.overflow.i32
+  b++;
+
+  // DEFAULT: add i32
+  ++a;
+  // DEFAULT: llvm.sadd.with.overflow.i32
+  ++b;
+
+  volatile extern int divisor;
+  // DEFAULT: %[[T0:.*]] = load i32, ptr %a
+  // DEFAULT-NEXT: %[[T1:.*]] = load volatile i32, ptr @divisor
+  // DEFAULT-NOT: br {{.*}} %handler.divrem_overflow
+  // DEFAULT: sdiv i32 %[[T0]], %[[T1]]
+  a/divisor;
+
+  // DEFAULT: %[[T0:.*]] = load i32, ptr %b
+  // DEFAULT-NEXT: %[[T1:.*]] = load volatile i32, ptr @divisor
+  // DEFAULT: br {{.*}} %handler.divrem_overflow
+  b/divisor;
 }
+
+// DEFAULT-LABEL: define {{.*}} @test2
+void test2(unsigned char __wrap a, unsigned char __nowrap b) {
+  // DEFAULT: add i8
+  (a + 1);
+  // DEFAULT: llvm.uadd.with.overflow.i8
+  (b + 1);
+
+  // DEFAULT: %[[T0:.*]] = load volatile i64, ptr @big
+  // DEFAULT: %[[TRUNC1:.*]] = icmp eq i64 {{.*}} %[[T0]]
+  // DEFAULT-NOT: br i1 %[[TRUNC1]], {{.*}} %handler.implicit_conversion
+  volatile extern unsigned long long big;
+  a = big;
+
+  // DEFAULT: %[[T1:.*]] = load volatile i64, ptr @big
+  // DEFAULT: %[[TRUNC2:.*]] = icmp eq i64 {{.*}} %[[T1]]
+  // DEFAULT: br i1 %[[TRUNC2]], {{.*}} %handler.implicit_conversion
+  b = big;
+}
+
+// DEFAULT-LABEL: define {{.*}} @test3
+void test3(void) {
+  volatile extern char __wrap a;
+  volatile extern short __wrap b;
+  // less-than-int arithmetic is possible when one or more wrapping types are
+  // present. When both operands are wrapping types, the larger of the two
+  // types should be used as the result of the arithmetic.
+
+  // DEFAULT: add i16
+  (a + b);
+
+  // nowrap has precedence over wrap, regardless of bit widths
+  volatile extern unsigned long long __wrap c;
+  volatile extern char __nowrap d;
+
+  // DEFAULT: %[[T0:.*]] = load volatile i64, ptr @c
+  // DEFAULT: %[[TRUNC1:.*]] = icmp eq i64 {{.*}} %[[T0]]
+  // DEFAULT: br i1 %[[TRUNC1]]
+  // DEFAULT: %[[T1:.*]] = load volatile i8, ptr @d
+  // DEFAULT-NEXT: @llvm.sadd.with.overflow.i8
+  (c + d);
+
+  volatile extern int __nowrap e;
+  volatile extern unsigned int __wrap f;
+
+  // DEFAULT: @llvm.ssub.with.overflow.i32
+  (e - f);
+}
+
+// TODO:
+// [x] unsigned types
+// [x] various bitwidths
+// [x] truncation
+// [x] negation
+// [x] competing OBT kinds (wrap vs nowrap)
+// -ftrapv, -fwrapv
+// SCL integration
+// -ftrapv-handler
