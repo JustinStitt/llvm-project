6eca1ec67164bc4e901d0b3df6400ae3f3be8cc8
Justin Stitt <justinstitt@google.com>
2025-05-15 14:38:58 -0700

    add cpp sema tests
    Signed-off-by: Justin Stitt <justinstitt@google.com>

---
+++ clang/test/Sema/attr-overflow-behavior.cpp
@@ -0,0 +1,63 @@
+// RUN: %clang_cc1 %s -Winteger-overflow -Wno-unused-value -verify -fsyntax-only
+
+typedef int __attribute__((overflow_behavior)) bad_arg_count; // expected-error {{'overflow_behavior' attribute takes one argument}}
+typedef int __attribute__((overflow_behavior(not_real))) bad_arg_spec; // expected-error {{'not_real' is not a valid argument to attribute 'overflow_behavior'}}
+typedef int __attribute__((overflow_behavior("not_real"))) bad_arg_spec_str; // expected-error {{'not_real' is not a valid argument to attribute 'overflow_behavior'}}
+typedef char* __attribute__((overflow_behavior("wrap"))) bad_type; // expected-warning {{'overflow_behavior' attribute cannot be applied to non-integer type char *}}
+
+typedef int __attribute__((overflow_behavior(wrap))) ok_wrap; // OK
+typedef long __attribute__((overflow_behavior(no_wrap))) ok_nowrap; // OK
+typedef unsigned long __attribute__((overflow_behavior("wrap"))) str_ok_wrap; // OK
+typedef char __attribute__((overflow_behavior("no_wrap"))) str_ok_nowrap; // OK
+
+void foo() {
+  (2147483647 + 100); // expected-warning {{overflow in expression; result is }}
+  (ok_wrap)2147483647 + 100; // no warn
+}
+
+// C++ stuff expects no warns
+typedef int __attribute__((overflow_behavior(wrap))) wrap_int;
+
+template <typename T>
+T bar(T a) {
+  return 1UL;
+}
+
+void f() {
+  wrap_int a = 4;
+  bar(a);
+}
+
+class TestOverload {
+  public:
+    int x;
+    TestOverload() = delete;
+    TestOverload(int x) : x(x) {}
+
+    void operator<<(int other) {
+      this->x += other;
+    }
+
+    void operator<<(char other) {
+      this->x += (int)other;
+    }
+};
+
+void test_overload1() {
+  wrap_int a = 4;
+  TestOverload TO(10);
+  TO << a;
+}
+
+int add_one(long a) { // expected-note {{candidate function}}
+  return (a + 1);
+}
+
+int add_one(char a) { // expected-note {{candidate function}}
+  return (a + 1);
+}
+
+void test_overload2(wrap_int a) {
+  // to be clear, this is the same ambiguity expected when using a non-OBT int type.
+  add_one(a); // expected-error {{call to 'add_one' is ambiguous}}
+}
