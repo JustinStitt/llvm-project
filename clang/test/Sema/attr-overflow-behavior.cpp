0b912724a66cf7c5a88c7fa5dd6f196194a4a137
Justin Stitt <justinstitt@google.com>
2025-07-10 18:09:50 -0700

    concat tests
    Signed-off-by: Justin Stitt <justinstitt@google.com>

--- clang/test/Sema/attr-overflow-behavior.cpp
+++ clang/test/Sema/attr-overflow-behavior.cpp
@@ -10,6 +10,10 @@ typedef long __attribute__((overflow_behavior(no_wrap))) ok_nowrap; // OK
 typedef unsigned long __attribute__((overflow_behavior("wrap"))) str_ok_wrap; // OK
 typedef char __attribute__((overflow_behavior("no_wrap"))) str_ok_nowrap; // OK
 
+struct struct_not_allowed {
+  int i;
+} __attribute__((overflow_behavior(wrap))); // expected-warning {{'overflow_behavior' attribute only applies to variables, typedefs, and non-static data members}}
+
 void foo() {
   (2147483647 + 100); // expected-warning {{overflow in expression; result is }}
   (ok_wrap)2147483647 + 100; // no warn
@@ -95,3 +99,17 @@ void test_overload_ambiguity() {
   overloadme(a); // expected-error {{call to 'overloadme' is ambiguous}}
 }
 
+void f(void) __attribute__((overflow_behavior(wrap))); // expected-warning {{'overflow_behavior' attribute cannot be applied to non-integer type 'void (void)'; attribute ignored}}
+
+typedef float __attribute__((overflow_behavior(wrap))) wrap_float; // expected-warning {{'overflow_behavior' attribute cannot be applied to non-integer type 'float'; attribute ignored}}
+
+void pointer_compatibility_test(int* i_ptr) {
+  __no_wrap int* nowrap_ptr;
+
+  // static_cast should fail.
+  nowrap_ptr = static_cast<__no_wrap int*>(i_ptr); // expected-error {{static_cast from 'int *' to '__no_wrap int *' is not allowed}}
+
+  // reinterpret_cast should succeed.
+  nowrap_ptr = reinterpret_cast<__no_wrap int*>(i_ptr);
+  (void)nowrap_ptr;
+}
